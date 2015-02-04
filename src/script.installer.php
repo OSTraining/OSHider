0077a969a0b265a397566e4731ab0bbfbf66c22d
Nick Savov <nick@iowawebcompany.com>
2015-02-03 18:52:53 -0600

    Initial code

---
+++ src/script.installer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @package   OSHider
+ * @contact   www.alledia.com, support@alledia.com
+ * @copyright 2015 Alledia.com, All rights reserved
+ * @license   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ */
+
+use Alledia\Installer\AbstractScript;
+
+defined('_JEXEC') or die();
+
+require_once 'library/Installer/include.php';
+
+class plgcontentoshiderInstallerScript extends AbstractScript
+{
+    /**
+     * @param string                     $type
+     * @param JInstallerAdapterComponent $parent
+     *
+     * @return void
+     */
+    public function postFlight($type, $parent)
+    {
+        parent::postFlight($type, $parent);
+
+        if (stripos($type, 'install') === false) {
+            $this->reorderThisPlugin();
+        }
+    }
+}
